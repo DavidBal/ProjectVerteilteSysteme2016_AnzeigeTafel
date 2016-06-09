cb4442d35f6bf14f4e47bab52e12dcc51a7cdc41
DavidBal <david_michael-baldauf@t-online.de>
2016-06-09 19:07:13 +0200

    Update

--- Client/src/client/ClientMain.java
+++ Client/src/client/ClientMain.java
@@ -12,7 +12,7 @@ import clientUISimpel.Konsole;
 public class ClientMain {
 
 	public static final int SERVER_PORT = 4690;
-	
+
 	/**
 	 * 
 	 * 
@@ -21,21 +21,21 @@ public class ClientMain {
 	public static void main(String[] args) {
 		try {
 			ServerConector test = new ServerConector("localhost", SERVER_PORT);
-			
-			//TODO Thread der das Auto-Uppdate der Daten übernimmt!
-			
+
+			// TODO Thread der das Auto-Uppdate der Daten übernimmt!
+
 			test.ping();
-			//test.isReachable();
+			// test.isReachable();
 			test.sendNewMessage("Hallo Welt!!");
-			test.auth("test", "test"); 	// Alles richtig
-			test.auth("Nein", "test");	// User falsch
-			test.auth("test", "Nein");	// PW falsch
-			test.auth("nein", "nein");	// Alles falsch
-			
-			//Ruft das Test Menue auf !!Konsole!!
+			test.auth("test", "test"); // Alles richtig
+			test.auth("Nein", "test"); // User falsch
+			test.auth("test", "Nein"); // PW falsch
+			test.auth("nein", "nein"); // Alles falsch
+
+			// Ruft das Test Menue auf !!Konsole!!
 			Konsole c = new Konsole(test);
 			c.hauptMenue();
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
