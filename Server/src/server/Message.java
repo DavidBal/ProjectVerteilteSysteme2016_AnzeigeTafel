dbde5cc5432a7fdfc90191d7d632b4d8739e544e
DavidBal <david_michael-baldauf@t-online.de>
2016-07-12 16:42:31 +0200

    Update

--- Server/src/server/Message.java
+++ Server/src/server/Message.java
@@ -1,28 +1,54 @@
 package server;
 
-import ControllCalls.ControllCalls;
-
 public class Message {
 
-	ControllCalls cc;
-	String s;
+	static String split = " </-/> ";
 
-	public Message(ControllCalls cc, String s) {
-		this.s = s;
-		this.cc = cc;
+	int id;
+	public int getId() {
+		return id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getAbteilung() {
+		return abteilung;
+	}
+
+	public int getLastchange() {
+		return lastchange;
+	}
+
+	String text;
+	String username;
+	String abteilung;
+	int lastchange;
+
+	public Message(int id, String text, String username, String abteilung, int lastchange) {
+		this.id = id;
+		this.text = text;
+		this.username = username;
+		this.abteilung = abteilung;
+		this.lastchange = lastchange;
 	}
 
 	public String toString() {
-		return cc.toString() + "|" + s + "|" + ControllCalls.END.toString();
+		String tmp = this.id + split + this.text + split + this.username + split + this.abteilung + split
+				+ this.lastchange;
+		return tmp;
+	}
+	
+	public static Message stringToMessage(String msg){
+		
+		String[]tmp = msg.split(split);
+		
+		return new Message(Integer.valueOf(tmp[0]),tmp[1],tmp[2],tmp[3],Integer.valueOf(tmp[4]));
 	}
 
-	public static Message stringToMessage(String message){
-		Message m = null;
-		
-		String[] feld = message.split("|");
-		
-		ControllCalls.stringToControllCall(feld[0]);
-		
-		return m;
-	}
 }
